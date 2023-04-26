7f0ab294bee759b4063444135ac0d9af76ffcad1
Pheeraphat Prisan <77052746+newpheeraphat@users.noreply.github.com>
2023-04-26 10:00:26 +0700

    fix: fix crossorigin error

--- cilent/src/App.js
+++ cilent/src/App.js
@@ -18,7 +18,7 @@ function App() {
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
       integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
-      crossorigin="anonymous"
+      crossOrigin="anonymous"
       />
       <div className="App" style={{backgroundColor: "white"}}>
         <div className="bg-lightslategray min-vh-100 min-vw-100 d-flex align-items-center justify-content-center">
